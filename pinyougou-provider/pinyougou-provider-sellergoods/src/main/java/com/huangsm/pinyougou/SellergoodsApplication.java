73b6adad3cb48c8707d91c1db0c27920d1c65131
h1261 <h1261109615@qq.com>
2018-11-30 18:03:20 +0800

    依赖死循环

---
+++ pinyougou-provider/pinyougou-provider-sellergoods/src/main/java/com/huangsm/pinyougou/SellergoodsApplication.java
@@ -0,0 +1,12 @@
+package com.huangsm.pinyougou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellergoodsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellergoodsApplication.class, args);
+    }
+}
