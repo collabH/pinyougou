41577ad8d66011f197372acf5ede39846add0f78
babybabywang <h1261109615@qq.com>
2018-12-01 19:28:41 +0800

    品优(解决死循环问题）

--- pinyougou-provider/pinyougou-provider-sellergoods/src/main/java/com/huangsm/pinyougou/SellergoodsApplication.java
+++ pinyougou-provider/pinyougou-provider-sellergoods/src/main/java/com/huangsm/pinyougou/SellergoodsApplication.java
@@ -1,9 +1,15 @@
 package com.huangsm.pinyougou;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class SellergoodsApplication {
 
     public static void main(String[] args) {
