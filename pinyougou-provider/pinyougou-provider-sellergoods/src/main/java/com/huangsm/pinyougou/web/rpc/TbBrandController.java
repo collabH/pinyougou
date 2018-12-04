e0de3cf3cc4f2490a43bc8c439891383a1c8cc8a
h1261 <h1261109615@qq.com>
2018-12-04 10:02:29 +0800

    商家管理后台（添加自定义swaggger2接口文档）

--- pinyougou-provider/pinyougou-provider-sellergoods/src/main/java/com/huangsm/pinyougou/web/rpc/TbBrandController.java
+++ pinyougou-provider/pinyougou-provider-sellergoods/src/main/java/com/huangsm/pinyougou/web/rpc/TbBrandController.java
@@ -1,7 +1,9 @@
-package com.huangsm.pinyougou.controller;
+package com.huangsm.pinyougou.web.rpc;
 
 import com.huangsm.pinyougou.domain.TbBrand;
 import com.huangsm.pinyougou.service.TbBrandService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api(value = "/tbBrand",tags = "品牌管理接口模块")
 @RestController
 @RequestMapping("/tbBrand")
 public class TbBrandController {
     @Autowired
     private TbBrandService brandService;
 
+    @ApiOperation(value = "查询全部品牌信息",notes ="查询全部品牌信息" )
     @GetMapping(value = "findAllBrands")
     public List<TbBrand> findBrands(){
         System.out.println("被调用了");
