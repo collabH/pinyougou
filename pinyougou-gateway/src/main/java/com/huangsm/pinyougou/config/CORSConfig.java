6f4f3c83b7a6bac230b0c9cc201e5cb3639122ce
h1261 <h1261109615@qq.com>
2018-12-21 14:32:34 +0800

    商家管理后台（没有LCN分布式事务版本）

--- pinyougou-gateway/src/main/java/com/huangsm/pinyougou/config/CORSConfig.java
+++ pinyougou-gateway/src/main/java/com/huangsm/pinyougou/config/CORSConfig.java
@@ -1,7 +1,5 @@
 package com.huangsm.pinyougou.config;
 
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -24,7 +22,9 @@ import reactor.core.publisher.Mono;
  */
 @Configuration
 public class CORSConfig {
-    //这里为支持的请求头，如果有自定义的header字段请自己添加（不知道为什么不能使用*）
+    /**
+     * 这里为支持的请求头，如果有自定义的header字段请自己添加（不知道为什么不能使用*）
+     */
     private static final String ALLOWED_HEADERS = "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN,token,username,client";
     private static final String ALLOWED_METHODS = "*";
     private static final String ALLOWED_ORIGIN = "*";
